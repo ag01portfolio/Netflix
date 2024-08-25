72c5e5f08233a50478da7570110bd512c1c4331f
Abhi - ROG <ag01portfolio@gmail.com>
2024-08-25 20:48:12 +0530

    Added List item + scroll functionality

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import './TitleCards'
-import CardData from '../../assets/cards/Cards_data'
+import './TitleCards.css'
+import card_data from '../../assets/cards/Cards_data'
 
 function TitleCards() {
     return (
         <div className="title-card">
             <h2>Popular on Netflix</h2>
-            <div className="card_list">
-                {CardData.map((card, index) => {
-                    return <div className="card">
-                        <img src={card.image} />
-                        <p>{card.name}</p>
+            <div className="card-list">
+                {card_data.map((card, index) => {
+                    return <div className="card" key={index}>
+                        <img src={card.image} alt="card_image" />
+                        {/* <p>{card.name}</p> */}
                     </div>
                 })}
             </div>
