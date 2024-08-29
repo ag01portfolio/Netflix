a0effbffd9cb2c3d7ff9dc9c9f69cb74cdf9771e
Abhi - ROG <ag01portfolio@gmail.com>
2024-08-29 10:51:41 +0530

    style [name, email, check box, placeholders, pass, help, rem, etc..]

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,20 +5,20 @@ import Logo from '../../assets/logo.png'
 function Login() {
   return (
     <div className='login'>
-      <img src={Logo} />
+      <img src={Logo} className='login-logo' />
       <div className="login-form">
         <h1>Sign Up</h1>
         <form>
           <input type='text' className='sign-up-name' placeholder='Your name' />
           <input type='email' className='sign-up-name' placeholder='email' />
           <input type='password' className='sign-up-name' placeholder='password' />
-          <input type='text' className='sign-up-name' placeholder='Your name' />
+          <button > Sign Up</button>
           <div className="form-help">
-            <div className='rember-mef'>
-
+            <div className='remember'>
+              <input type="checkbox" />
+              <label htmlFor=''>Remember me</label>
             </div>
             <p>Need Help</p>
-
           </div>
         </form>
       </div>
