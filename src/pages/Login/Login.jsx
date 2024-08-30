cec05689d2d91711ce2e63f45583b922cab16ad9
Abhi - ROG <ag01portfolio@gmail.com>
2024-08-30 09:08:02 +0530

    Adding Sign In and Sign Up logic

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import Logo from '../../assets/logo.png'
 
 function Login() {
+  const [signState, setSignState] = useState('Sign In')
+  const isLoggedIn = signState === 'Sign In'
   return (
     <div className='login'>
       <img src={Logo} className='login-logo' />
       <div className="login-form">
-        <h1>Sign Up</h1>
+        <h1>{signState}</h1>
         <form>
-          <input type='text' className='sign-up-name' placeholder='Your name' />
+          {!isLoggedIn && <input type='text' className='sign-up-name' placeholder='Your name' />}
           <input type='email' className='sign-up-name' placeholder='email' />
           <input type='password' className='sign-up-name' placeholder='password' />
-          <button > Sign Up</button>
+          <button >{signState}</button>
           <div className="form-help">
             <div className='remember'>
               <input type="checkbox" />
@@ -21,6 +23,10 @@ function Login() {
             <p>Need Help</p>
           </div>
         </form>
+        <div className="form-switch">
+          {isLoggedIn ? <p>New to Netflix<span onClick={() => { setSignState("Sign up") }}>Sing up now</span></p>
+            : <p>Already have Account<span onClick={() => { setSignState("Sign In") }}>Sing in now</span></p>}
+        </div>
       </div>
     </div>
   )
