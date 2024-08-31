5e03cde6f7c36ce8d574b6e67b38a111f428ea57
Abhi - ROG <ag01portfolio@gmail.com>
2024-08-31 19:08:52 +0530

    Fetch movies from API

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -5,6 +5,21 @@ import card_data from '../../assets/cards/Cards_data'
 function TitleCards({ title, category }) {
     // let tileTitle = title ? title : "Top on Netflix"
     const tileTitle = title ? title : "Top on Netflix";
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYTJmNTRlMDVmNDJiNDQ2ODZmOGM1ZDRjNGM2MzI3OSIsIm5iZiI6MTcyNTExMDIzNy43NDQ1MTksInN1YiI6IjY2ZDE0MTFiYjc0ZjAzZDE4ZTcwNmFiMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VvPNwsGuY1nB6OIK-xcCwpyxc3BAHqSEA6cWcSJCSBg'
+        }
+    };
+
+    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+
+
     return (
         <div className="title-card">
             <h2>{tileTitle}</h2>
