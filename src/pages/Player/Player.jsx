f3581f2bb2da165e27c8327d7b8a962a7708d63a
Abhi - ROG <ag01portfolio@gmail.com>
2024-09-03 00:46:32 +0530

    back_arrow_icon

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
 
 const Player = () => {
   return (
-    <div className='player'>Player</div>
+    <div className='player'>
+      <img src={back_arrow_icon} />
+      <iframe></iframe>
+    </div>
   )
 }
 
