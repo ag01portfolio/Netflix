15901850fe78b71883e836e872db694058a3ac33
Abhi - ROG <ag01portfolio@gmail.com>
2024-09-04 14:48:58 +0530

    Player Path

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Home from './pages/Home/Home'
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login/Login'
+import Player from './pages/Player/Player'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/player/:id" element={<Player />} />
       </Routes>
     </div>
   )
