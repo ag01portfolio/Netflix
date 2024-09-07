156fe6f5438f8129ba077437a1e42b3f75249f50
Abhi - ROG <ag01portfolio@gmail.com>
2024-09-07 22:30:02 +0530

    url of the video

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -3,13 +3,17 @@ import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
 
 const Player = () => {
+  const key = "5OPShZJkJR8?si=-gGpogklRJoyhjVv"
+  const BaseURL = 'https://www.youtube.com/embed/'
+  const url = BaseURL + key
+
   return (
     <div className='player'>
       <img src={back_arrow_icon} />
       <iframe
         height={'90%'}
         width={'90%'}
-        src='https://www.youtube.com/embed/5OPShZJkJR8?si=-gGpogklRJoyhjVv'
+        src={url}
         title='trailer'
         frameBorder={'0'}
         allowFullScreen={true}
